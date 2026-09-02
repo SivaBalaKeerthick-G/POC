5af0d17ae74dd109057e13014a28be3305be6a5e
Siva Bala Keerthick G <sivabalakeerthick.g@cognizant.com>
2026-09-02 17:27:25 +0530

    Auth0 setup

---
+++ src/app/components/login/login.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+  private authService = inject(AuthService);
+
+  login(): void {
+    this.authService.login();
+  }
+}
